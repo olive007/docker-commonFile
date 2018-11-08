1213562d678c4d2b64bc7818bf61cc4216714a54
Olivier SECRET <olivier.secret@catalyst-au.net>
2018-11-08 17:03:26 +1100

    fix permission error

--- usr/local/script/entry-point.d/apache.sh
+++ usr/local/script/entry-point.d/apache.sh
@@ -7,8 +7,8 @@ echo "ServerName "`hostname -i` >> /etc/apache2/apache2.conf
 [ -L /home/$CONTAINER_USER_NAME/www ] || ln -s /var/www/html /home/$CONTAINER_USER_NAME/www
 
 # Set the new user as the user who run apache
-echo "export APACHE_RUN_USER=$CONTAINER_USER_NAME" >> /etc/apache2/envvars
-echo "export APACHE_RUN_GROUP=$CONTAINER_USER_NAME" >> /etc/apache2/envvars
+sed -i "s/^export APACHE_RUN_USER=.*$/export APACHE_RUN_USER=$CONTAINER_USER_NAME/" /etc/apache2/envvars
+sed -i "s/^export APACHE_RUN_GROUP=.*$/export APACHE_RUN_GROUP=$CONTAINER_USER_NAME/" /etc/apache2/envvars
 
 # Fix permision error
-chown $CONTAINER_USER_NAME:$CONTAINER_USER_NAME /var/www/*
+chown -R $CONTAINER_USER_NAME:$CONTAINER_USER_NAME /var/www/
