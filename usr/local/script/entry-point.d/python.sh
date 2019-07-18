c510beb31d191270b879718b213fd030bb473d4c
Olivier SECRET <noreply@noreply.com>
2019-07-18 16:39:43 -0400

    fix bug 'creating user directory because of volume mounted'

--- usr/local/script/entry-point.d/python.sh
+++ usr/local/script/entry-point.d/python.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-
-if [ -f "/home/$CONTAINER_USER_NAME/requirements.txt" ]; then
+if [ -f "/PYTHON/requirements.txt" ]; then
 	# Install the required python packages
-	pip install -r /home/$CONTAINER_USER_NAME/requirements.txt
+	pip install -r /PYTHON/requirements.txt
 fi
 
-# Create a empty directory to mount the source folder into it
-mkdir /home/$CONTAINER_USER_NAME/python-app
+# Create a link into the user directory
+ln -s /PYTHON/requirements.txt /home/$CONTAINER_USER_NAME/requirements.txt
+ln -s /PYTHON/app /home/$CONTAINER_USER_NAME/app
+
+chown $CONTAINER_USER_NAME:$CONTAINER_USER_NAME -R /PYTHON
