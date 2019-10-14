a93d1de95b92d9150a1cfd740073e45bdce175ef
Olivier SECRET <olive007@users.noreply.github.com>
2019-10-14 20:55:48 +0200

    Add configurable enviroment for PHP XDEBUG

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # This repository containts all the file copied into my Docker container
 
 It's more easy to maintain them into only 1 repository
+
+Xdebug configuration can be set with any XDEBUG_ enviroment variable
--- usr/local/script/entry-point.d/php.sh
+++ usr/local/script/entry-point.d/php.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-XDEBUG_HOST=`hostname -i | cut -d. -f1-3`.1
+# Get the PHP version
 PHP_VERSION=`php -r "echo PHP_VERSION;" | cut -c 1,3 --output-delimiter="."`
 
-# Set configuration for XDEBUG
-echo "xdebug.remote_enable=on" >> /etc/php/$PHP_VERSION/mods-available/xdebug.ini 
-echo "xdebug.idekey=$XDEBUG_IDEKEY" >> /etc/php/$PHP_VERSION/mods-available/xdebug.ini
-echo "xdebug.remote_host=$XDEBUG_HOST" >> /etc/php/$PHP_VERSION/mods-available/xdebug.ini
-echo "xdebug.remote_port=9000" >> /etc/php/$PHP_VERSION/mods-available/xdebug.ini
+# Get the network of the container and set the host to the firt ip
+XDEBUG_HOST=`hostname -i | cut -d. -f1-3`.1
+# Get all enviroment variable starting by 'XDEBUG_'
+XDEBUG_ENV_CONFIGS=`printenv | grep "^XDEBUG_"`
+# Set the xdebug file location
+XDEBUG_FILE=/etc/php/$PHP_VERSION/mods-available/xdebug.ini
+
+# Set default configuration for XDEBUG
+echo "xdebug.remote_enable=on" >> $XDEBUG_FILE
+echo "xdebug.remote_host=$XDEBUG_HOST" >> $XDEBUG_FILE
+echo "xdebug.remote_port=9001" >> $XDEBUG_FILE
+echo "xdebug.remote_connect_back=1" >> $XDEBUG_FILE
+echo "xdebug.remote_autostart=1" >> $XDEBUG_FILE
+
+# For each xdebug enviroment variable set the config will be set
+for xDebugConfig in $XDEBUG_ENV_CONFIGS; do
+
+    config=`echo $xDebugConfig | cut -d= -f1 | tr '[:upper:]' '[:lower:]'`
+    config=${config:7}
+    value=`echo $xDebugConfig | cut -d= -f2-`
+
+    echo "Setting PHP xdebug.$config to $value"
+
+    echo "xdebug.$config=$value" >> $XDEBUG_FILE
+    
+done
+
+
 
 # Configure the user for PHP FPM
 sed -i "s/^user =.*$/user = $CONTAINER_USER_NAME/" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
